d2881dd707532ca9c2a1056c66985ba870b52d4a
nvcc1701 <nguyenlamanh1701@gmail.com>
2024-06-29 20:58:53 +0700

    d

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
+# Get the absolute path of the directory containing this script
 shDir=$(cd $(dirname $0) && pwd)
+
+# Set the path for the .env file
 ENVFILE=${shDir}/docker/.env
+
+# If the .env file doesn't exist, copy the .env.dist file to it
 if [ ! -f "$ENVFILE" ]; then
   cp ${shDir}/docker/.env.dist ${ENVFILE}
 fi
 
+# Check if Docker is installed
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
 fi
 
-docker compose --file "docker/docker-compose.yml" up -d --build
+# Start the Docker services defined in docker-compose.yml with detached mode and rebuilding images
+docker-compose -f "docker/docker-compose.yml" up -d --build
